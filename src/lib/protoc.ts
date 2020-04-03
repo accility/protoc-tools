548c382bebe29c5654570b35f4ebbafe78797102
Martin Skogevall <martin.skogevall@accility.se>
2020-04-03 08:28:42 +0200

    Make parameters in ProtocOptions optional.

--- src/lib/protoc.ts
+++ src/lib/protoc.ts
@@ -2,30 +2,33 @@ import * as path from 'path';
 import { spawn } from 'child_process';
 
 export interface PluginOptions {
-    name: string;
-    path: string;
+  name: string;
+  path: string;
 }
 
 export interface ProtocOptions {
-    includeDirs: string[];
-    files: string[];
-    outOptions: string;
-    plugin: PluginOptions;
+  includeDirs: string[];
+  files: string[];
+  outOptions?: string;
+  plugin?: PluginOptions;
 }
 
 // Convert the protoc options object to the corresponding command line arguments
 export function protoc(options: ProtocOptions) {
   const defaultProtoDir = path.resolve(__dirname, '../../native/include');
   const protoDirs = [defaultProtoDir, ...options.includeDirs].map(e => `-I${e}`);
-  const pluginArg = `--plugin=protoc-gen-${options.plugin.name}=${options.plugin.path}`;
-  const outOptions = `--${options.plugin.name}_out=${options.outOptions ?? '.'}`;
 
   const args = [
-    pluginArg,
-    outOptions,
+    path.resolve(__dirname, '../bin/protoc-cli.js'),
     ...protoDirs,
     ...options.files
   ];
 
-  spawn('node', [path.resolve(__dirname, '../bin/protoc-cli.js'), ...args], {stdio: 'inherit'});
+  const plugin = options.plugin;
+  if (plugin) {
+    args.push(`--plugin=protoc-gen-${plugin.name}=${plugin.path}`);
+    args.push(`--${plugin.name}_out=${options.outOptions ?? '.'}`);
+  }
+
+  spawn('node', args, { stdio: 'inherit' });
 };
